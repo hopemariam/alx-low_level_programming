f3ed6f1f44e8a99506f4e9c6afc87b9d3556338d
hopemariam <hope2006ng.ts@gmail.com>
2022-09-13 10:29:01 -0700

    my 1-alphabet commit

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}       
