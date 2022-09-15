43204cd3bb81b29edeb4ab1c040cc55d45662726
hopemariam <hope2006ng.ts@gmail.com>
2022-09-15 14:39:36 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-/**	
- *	while (i < 10)
- *	{
- *		putchar(i);
- *	}
- */	
+
+	/*while (i < 10) - infinite loop*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
